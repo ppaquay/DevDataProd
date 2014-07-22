a553d2030df17dd66bd2357a334fc0d78cc895f0
ppaquay <p.paquay@linuxaddict.be>
2014-07-22 20:40:01 +0200

    Draft with documentation.

--- PredApp/server.R
+++ PredApp/server.R
@@ -6,7 +6,9 @@ model <- lm(eruptions ~ waiting, data = faithful)
 
 shinyServer(
     function(input, output) {
-        output$out_wait_time <- renderPrint({input$in_wait_time})
+        output$out_wait_time <- renderPrint({input$in_wait_time})   
+        #output$prediction <- renderPrint({predict(model, data.frame(waiting = input$in_wait_time))[[1]]})
+        #wait <- reactive({input$in_wait_time})
         wait <- reactive({
             validate(
                 need((input$in_wait_time <= 96) & (input$in_wait_time >= 43), "Entered waiting time off limits !")
@@ -17,16 +19,13 @@ shinyServer(
         output$prediction <- renderPrint({pred()})
         output$reg_plot <- renderPlot({
             par(mar = c(5.1, 4.1, 0, 2.1))
-            plot(eruptions ~ waiting, data = faithful, xlab = "Winting time to next eruption (in mins)", ylab = "Eruption duration (in mins)")
+            plot(eruptions ~ waiting, data = faithful, xlab = "Waiting time to next eruption (in mins)", ylab = "Eruption duration (in mins)")
             points(wait(), pred(), col = "red", pch = 19)
             segments(wait(), 0, wait(), pred(), col = "red", lty = 2)
             segments(0, pred(), wait(), pred(), col = "red", lty = 2)
             text(40, pred(), round(pred(), digits = 2), adj = c(-0.5,-0.5), col = "red")
             text(wait(), 1.5, round(wait(), digits = 2), adj = c(-0.5, 0), col = "red")
-            if (input$smooth == "W") {
-                abline(model, col = "blue")
-            }
-            if (input$smooth == "Wo") {}
+            abline(model, col = "blue")
         })
     }
 )
--- PredApp/ui.R
+++ PredApp/ui.R
@@ -1,35 +1,37 @@
 library(shiny)
 
-shinyUI(fixedPage(theme = "bootstrap.css",
-    
-    tags$head(
-        tags$style(HTML("
-      .shiny-output-error-validation {
-        color: red;
-        font-weight: bold;
-      }
-    "))
-    ),
-    
-    titlePanel(strong(em("Prediction of the duration of the eruption of Old Faithful geyser in Yellowstone National Park, Wyoming, USA"))),
-    p(em("This application predicts the duration of an eruption as a function of the waiting time to the next eruption. The waiting times must be between 43 mins and 96 mins to avoid extrapolation.")),
-    sidebarLayout(
-        sidebarPanel(
-            h4("Waiting time to next eruption"),
-            numericInput("in_wait_time", "Enter the desired waiting time (in mins)", 70, min = 43, max = 96, step = 1),
-            h4("With or without regression line ?"),
-            selectInput("smooth", "Do you want to draw the plot with or without the regression line ?", list("With" = "W", "Without" = "Wo")),
-            p(),
-            submitButton("Submit")
-            ),
-        mainPanel(
-            h4("Waiting time entered (in mins) :"),
-            verbatimTextOutput("out_wait_time"),
-            h4("Prediction of eruption duration (in mins) :"),
-            verbatimTextOutput("prediction"),
-            h4("Plot of eruption duration vs waiting time to next eruption :"),
-            conditionalPanel(condition = "InputId == 'smooth'", plotOutput("reg_plot"))
+#shinyUI(fixedPage(theme = "bootstrap.css",
+shinyUI(navbarPage(em("Duration of Old Faithful geyser eruption"),
+    tabPanel("Application",
+        titlePanel(strong(em("Prediction of the duration of the eruption of Old Faithful geyser in Yellowstone National Park, Wyoming, USA"))),
+        #p(em("This application predicts the duration of an eruption as a linear function of the waiting time to the next eruption. The waiting times must be between 43 mins and 96 mins to avoid extrapolation. You may draw the plot of eruption duration vs. waiting time to next eruption with or without regression line.")),
+        sidebarLayout(
+            sidebarPanel(
+                h4("Waiting time to next eruption"),
+                numericInput("in_wait_time", "Enter the desired waiting time (in mins)", 70, min = 43, max = 96, step = 1),
+                #h4("With or without regression line ?"),
+                #selectInput("smooth", "Do you want to draw the plot with or without the regression line ?", list("With" = "W", "Without" = "Wo")),
+                p(),
+                submitButton("Submit")
+                ),
+            mainPanel(
+                h4("Waiting time entered (in mins) :"),
+                verbatimTextOutput("out_wait_time"),
+                h4("Prediction of eruption duration (in mins) :"),
+                verbatimTextOutput("prediction"),
+                h4("Plot of eruption duration vs waiting time to next eruption :"),
+                plotOutput("reg_plot")
+                )
             )
-        )
+        ),
+    tabPanel("Documentation",
+            h4("Synopsis"),
+            p("This application predicts the duration of an eruption as a linear function of the waiting time to the next eruption. The linear function is simply the regression line of the duration of the eruption vs the waiting time to the next eruption."),
+            h4("How it works"),
+            p("You have to enter a waiting time (in minutes) to the next eruption in the left-hand panel and then click on the Submit button. The waiting times entered must be between 43 mins and 96 mins to avoid extrapolation. If this is not the case the application will display the error message : Entered waiting time off limits !"),
+            h4("Results"),
+            p("The predicted duration of the eruption (in minutes) obtained from the waiting time entered will be displayed in the main panel as will be a reminder of the waiting time entered."),
+            p("Below you will also find a plot of eruption duration vs waiting time to next eruption. The regression line used for predicting the duration is also displayed (in blue) as are the coordinates of the prediction (in red).")
+    )
     )
 )
